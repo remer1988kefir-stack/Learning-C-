ff517b1ec5d02aa1ff71827f1ff07eb0214ebd05
Nonezzz <remer1988kefir@gmail.com>
2026-09-20 19:14:46 +0300

    Complete first C# user profile practice

--- First launch/Program.cs
+++ First launch/Program.cs
@@ -1,5 +1,36 @@
-﻿string name = "Benji";
-int age = 20;
+﻿Console.WriteLine("Как вас зовут?");
+string name = Console.ReadLine();
+while (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Ты ебень");
+    name = Console.ReadLine();
+}
+Console.WriteLine("Сколько вам лет?");
+int age = int.Parse(Console.ReadLine());
 
-Console.WriteLine(name);
-Console.WriteLine(age);
+Console.WriteLine("В каком городе вы живете?");
+string city = Console.ReadLine();
+
+
+Console.WriteLine("===== Информация о пользователе =====");
+Console.WriteLine($"Имя:  {name}");
+Console.WriteLine($"Возраст:  {age}");
+Console.WriteLine($"Город:  {city}");
+Console.WriteLine("=====================================");
+
+if (age < 0 || age > 150)
+{
+    Console.WriteLine("Статус: Неправильное значение");
+}
+else if (age <= 12)
+{
+    Console.WriteLine("Статус: Ребенок");
+}
+else if (age >= 13 && age <= 17)
+{
+    Console.WriteLine("Статус: Подросток");
+}
+else
+{
+    Console.WriteLine("Статус: Взрослый");
+}
